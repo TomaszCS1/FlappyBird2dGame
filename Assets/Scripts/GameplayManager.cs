0430c04c71edbb1fccbbf5083a5f84f3c48592f6
T <tomasz.k.tarnowski@gmail.com>
2023-02-06 09:41:18 +0100

    obstacle prefab added

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -6,52 +6,67 @@ using UnityEngine;
 public class GameplayManager : Singleton<GameplayManager> 
 {
 
-    public float TerrainWidth = 42.0f;
+    float TerrainWidth = 42.0f;
+    float obstacleDistance = 12.0f;
 
 
     public GameObject TerrainPrefab;
+    public GameObject obstaclePrefab;
 
     public BirdController m_bird;
 
     private List<GameObject> SpawnedTerrain;
+    private List<GameObject> SpawnedObstacle;
 
     private float terrainSpawnXLocation;
 
-    GameObject prefabInstance;
+    GameObject prefabTerrainInstance;
+    GameObject prefabObstacleInstance;
+
+    private float birdPosition;
+    private float birdLastObstacleDistance;
 
 
     // Start is called before the first frame update
     void Start()
     {
         SpawnedTerrain = new List<GameObject>();
-
         if (SpawnedTerrain.Count == 0)
         {
 
-            prefabInstance = GameObject.Instantiate(TerrainPrefab, new Vector3(TerrainWidth*1.0f, 0, 0), Quaternion.identity);
+            prefabTerrainInstance = GameObject.Instantiate(TerrainPrefab, new Vector3(TerrainWidth, 0, 0), Quaternion.identity);
            
-            SpawnedTerrain.Add(prefabInstance);
+            SpawnedTerrain.Add(prefabTerrainInstance);
         }
 
-
+        SpawnedObstacle = new List<GameObject>();
+        if (SpawnedObstacle.Count == 0)
+        {
+            obstacleDistance = Random.Range( obstacleDistance-2f, obstacleDistance+2f);
+            prefabObstacleInstance = GameObject.Instantiate(obstaclePrefab, new Vector3(obstacleDistance, 0, 0), Quaternion.identity);
+           
+            SpawnedObstacle.Add(prefabObstacleInstance);
+        }
+         birdPosition = m_bird.transform.position.x;
+         birdLastObstacleDistance = birdPosition;
+       
 
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+               
         
-        var birdPosition = m_bird.transform.position.x;
         var lastTerrrainPosition = SpawnedTerrain[SpawnedTerrain.Count - 1].transform.position.x;
         var newTerrainPosition = (lastTerrrainPosition + (TerrainWidth* 0.5f));
            
-        if ((birdPosition - lastTerrrainPosition ) > 0 && birdPosition > TerrainWidth)
+          if ((birdPosition - lastTerrrainPosition ) > 0 && birdPosition > TerrainWidth)
         {
 
             //instantiates prefab: Terrain positioned on the end of previous prefab 
-            prefabInstance = GameObject.Instantiate(TerrainPrefab, new Vector3(newTerrainPosition, 0, 0), Quaternion.identity);
-            SpawnedTerrain.Add(prefabInstance);
+            prefabTerrainInstance = GameObject.Instantiate(TerrainPrefab, new Vector3(newTerrainPosition, 0, 0), Quaternion.identity);
+            SpawnedTerrain.Add(prefabTerrainInstance);
 
 
             if (SpawnedTerrain.Count > 0 && birdPosition - SpawnedTerrain[0].transform.position.x - TerrainWidth*0.5> 0)
@@ -59,6 +74,30 @@ public class GameplayManager : Singleton<GameplayManager>
                 GameObject.Destroy(SpawnedTerrain[0]);
 
                 SpawnedTerrain.RemoveAt(0);
+
+            }
+
+        }
+        
+        var lastObstaclePosition = SpawnedObstacle[SpawnedObstacle.Count- 1].transform.position.x;
+        var newObstaclePosition = lastObstaclePosition + obstacleDistance;
+        
+        birdLastObstacleDistance =  lastObstaclePosition - birdPosition;
+
+        if (birdLastObstacleDistance>obstacleDistance
+            )
+        {
+            
+            //instantiates prefab: Terrain positioned on the end of previous prefab 
+             prefabObstacleInstance = GameObject.Instantiate(obstaclePrefab, new Vector3(newObstaclePosition , 0, 0), Quaternion.identity);
+            SpawnedObstacle.Add(prefabObstacleInstance);
+
+
+            if (SpawnedObstacle.Count > 0 && birdPosition - SpawnedObstacle[0].transform.position.x - TerrainWidth*0.5> 0)
+            {
+                GameObject.Destroy(SpawnedObstacle[0]);
+
+                SpawnedObstacle.RemoveAt(0);
 
             }
 
--- Assets/Scripts/ObstacleController.cs
+++ Assets/Scripts/ObstacleController.cs
@@ -28,21 +28,34 @@ public class ObstacleController : MonoBehaviour
         UpColumn = transform.Find("UpColumn").gameObject.GetComponent<SpriteRenderer>();
         DownColumn = transform.Find("DownColumn").gameObject.GetComponent<SpriteRenderer>();
       
+        UpColumn.size= new Vector2(5, 22);
+
+        UpdateObstacleParams();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+                
+    }
+
+    void UpdateObstacleParams()
+    {
         GapHeight = Mathf.Clamp(GapHeight, 5f, 10F);
         GapMidpoint = Mathf.Clamp(GapMidpoint, -3f, 4F);
+
+        GapMidpoint = UnityEngine.Random.Range(-3f, 3f);
+        GapHeight = UnityEngine.Random.Range(5f, 9f);
 
         transform.position = new Vector3(0, 1, 0) * GapMidpoint;
 
         UpColumn.transform.position = new Vector3(0, 1, 0) * (UpColumn.size.y / 2 + GapMidpoint + GapHeight / 2);
         Debug.Log(" UpColumn.size.y= " + UpColumn.size.y + " GapMidpoint: " + GapMidpoint + "GapHeight/2: " + GapHeight / 2);
 
-        DownColumn.transform.position = new Vector3(0, 1, 0) * (GapMidpoint - GapHeight/2 - DownColumn.size.y / 2);
+        DownColumn.transform.position = new Vector3(0, 1, 0) * (GapMidpoint - GapHeight / 2 - DownColumn.size.y / 2);
 
-        
     }
+
+
 }
